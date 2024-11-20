6922c5eaae76724ddc7fb24c05b19bbe24854752
Jason L. Lind <lind@yahoo.com>
2024-11-20 01:48:34 -0600

    json malformation fixes

--- Programming.Team.AI/ResumeEnricher.cs
+++ Programming.Team.AI/ResumeEnricher.cs
@@ -29,7 +29,7 @@ namespace Programming.Team.AI
                 if (config.HideSkillsNotInJD)
                 {
                     string? postingSkills = await ChatGPT.GetRepsonse("extract as many skill keywords as possible from user message in json format {\\\"skill\\\":\\\"name\\\"}",
-                        posting.Details, token: token);
+                        posting.Details.Replace("\"", "\\\""), token: token);
                     if (postingSkills != null)
                     {
                         postingSkills = postingSkills.Replace("```json", "").Replace("```", "");
@@ -55,20 +55,20 @@ namespace Programming.Team.AI
                 }
                 if(!string.IsNullOrWhiteSpace(resume.User.Bio))
                 {
-                    resume.User.Bio = await ChatGPT.GetRepsonse($"Tailor the user message biography to the following requirement, limit result to three paragraphs stick to what you know, don't make things up: {posting.Details}", resume.User.Bio, token: token);
+                    resume.User.Bio = await ChatGPT.GetRepsonse($"Tailor the user message biography to the following requirement, limit result to three paragraphs stick to what you know, don't make things up: {posting.Details.Replace("\"", "\\\"")}", resume.User.Bio.Replace("\"", "\\\""), token: token);
                 }
                 foreach(var position in resume.Positions)
                 {
                     if (!string.IsNullOrWhiteSpace(position.Description))
                     {
-                        var match = await ChatGPT.GetRepsonse($"Indicate a percent match, returning a single value in \\\"#%\\\", for the user message to the following job description: {posting.Details}", position.Description, token: token);
+                        var match = await ChatGPT.GetRepsonse($"Indicate a percent match, only responding with a single value in \\\"%\\\", for the user message to the following job description: {posting.Details.Replace("\"", "\\\"")}", position.Description.Replace("\"", "\\\""), token: token);
                         if (match != null)
                         {
                             double mtch = double.Parse(match.Replace("%", "")) / 100;
                             if (mtch >= (config.MatchThreshold ?? 0.45))
                             {
                                 double length = mtch*10 * (config.TargetLengthPer10Percent ?? 200);
-                                position.Description = await ChatGPT.GetRepsonse($"tailor user message, at a length of no more than {length} characters, to the following job requirement sticking to the facts included in the user message, do not be creative IF A TECHNOLOGY IS NOT MENTIONED IN THE USER MESSAGE DO NOT INCLUDE IT IN THE SUMMARY!!!!", position.Description, token: token);
+                                position.Description = await ChatGPT.GetRepsonse($"tailor user message, resulting in a total text length of no more than {length} characters, to the following job requirement sticking to the facts included in the user message, do not be creative IF A TECHNOLOGY IS NOT MENTIONED IN THE USER MESSAGE DO NOT INCLUDE IT IN THE SUMMARY!!!!", position.Description.Replace("\"", "\\\""), token: token);
                             }
                             else
                                 position.Description = "";
