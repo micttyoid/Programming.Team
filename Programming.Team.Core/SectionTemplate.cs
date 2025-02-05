758fe99c235b276f8c231c709a5b129260b05a79
Jason L. Lind <lind@yahoo.com>
2025-02-04 23:28:55 -0600

    select section template in resume configuration

---
+++ Programming.Team.Core/SectionTemplate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming.Team.Core
+{
+    public interface ISectionTemplate : IEntity<Guid>
+    {
+        ResumePart SectionId { get; set; }
+        string Name { get; set; }
+        string Template { get; set; }
+    }
+    public class SectionTemplate : Entity<Guid>, ISectionTemplate
+    {
+        public ResumePart SectionId { get; set; }
+        public string Name { get; set; } = null!;
+        public string Template { get; set; } = null!;
+    }
+}
